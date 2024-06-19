4555c3497677971dc9074d12e43ebe37f36a6111
Sankalp Pimpalkar <SankalpPimpalkar23@gmail.com>
2024-06-19 16:22:13 +0530

    project link not working [FIXED]

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ const projects = [
         project_name: 'AI Chat Bot',
         project_descriptions: 'This project showcases the development of an intelligent AI chatbot utilizing the Gemini API, designed to answer user questions with high accuracy and responsiveness. The chatbot is capable of understanding and processing natural language inputs, providing detailed and contextually relevant responses.',
         techstack: ['Reactjs', 'Tanstack Query', 'React-router-dom', 'Gemini API', 'Redux'],
-        project_link: 'ai-chat-bot-zeta.vercel.app/welcome',
+        project_link: 'https://ai-chat-bot-zeta.vercel.app/welcome',
         project_source_code: 'https://github.com/Sankalp-Pimpalkar/AI-Chat-Bot'
     },
 ]
